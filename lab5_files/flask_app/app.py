6770e539d0f2bbad734a0e6bc998cbb1b8630e45
kwakuNti <159504184+kwakuNti@users.noreply.github.com>
2024-10-11 16:41:08 +0000

    Refactor docker-compose.yml to include MongoDB service and update database configurations

--- lab5_files/flask_app/app.py
+++ lab5_files/flask_app/app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 import redis
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 r = redis.Redis(host="redis", port=6379)
-
+mongo_client = MongoClient(host="mongo", port=27017)
+mongo_db = mongo_client["mydatabase"]
+visits_collection = mongo_db["visits"]
 
 @app.route("/")
 def home():
     count = r.incr("hits")
-    return f"This page has been visited {count} times."
-
+    visits_collection.insert_one({"visit_number": count})
+    return f"This page has been visited {count} times. Visit data saved to MongoDB."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
